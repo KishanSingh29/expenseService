708ffbbd4da0e856d690244ddb090645b6bfc8c0
KishanSingh29 <learnwithkishu2@gmail.com>
2025-12-18 21:25:44 +0530

    finally clear all error and make it expenservices

--- src/main/java/com/expense/service/ServiceApplication.java
+++ src/main/java/com/expense/service/ServiceApplication.java
@@ -1,13 +1,24 @@
 package com.expense.service;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServiceApplication {
 
+	@Autowired
+	private Environment env;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
 	}
 
+	@PostConstruct
+	public void printKafkaBootstrap() {
+		System.out.println("🔥 ENV Bootstrap: " + env.getProperty("spring.kafka.bootstrap-servers"));
+		System.out.println("🔥 System Bootstrap: " + System.getProperty("spring.kafka.bootstrap-servers"));
+	}
 }
